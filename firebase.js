14657326054543ee43cac15c170c79442c4d9510
Lars Jönsson <crymeat@me.com>
2019-07-30 15:55:35 +0200

    deleted some keys :)

--- firebase.js
+++ firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCnsrJrnUkKEdJrV9WDR-vxZeuuA8Q-2W4",
-  authDomain: "react-todo-4bba4.firebaseapp.com",
-  databaseURL: "https://react-todo-4bba4.firebaseio.com",
-  projectId: "react-todo-4bba4",
-  storageBucket: "react-todo-4bba4.appspot.com",
-  messagingSenderId: "958431706722",
-  appId: "1:958431706722:web:65f902755d27d658"
+
 });
 
 const db = firebaseApp.firestore();
